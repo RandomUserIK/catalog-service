759e1d5affbc58f5b9bdc7a61ce60363c559aa35
RandomUserIK <kovac.ivan97@icloud.com>
2024-09-27 23:01:23 +0200

    feat(chapter-5): implement book entity and add connection to the db

--- src/main/kotlin/com/polarbookshop/catalogservice/domain/InMemoryBookRepository.kt
+++ src/main/kotlin/com/polarbookshop/catalogservice/domain/InMemoryBookRepository.kt
@@ -3,29 +3,29 @@ package com.polarbookshop.catalogservice.domain
 import org.springframework.stereotype.Repository
 import java.util.concurrent.ConcurrentHashMap
 
-@Repository
-class InMemoryBookRepository : BookRepository {
-    companion object {
-        private val books = ConcurrentHashMap<String, Book>()
-    }
-
-    override fun findAll(): Iterable<Book> =
-        books.values
-
-    override fun findByIsbn(isbn: String): Book? =
-        books[isbn]
-
-    override fun existsByIsbn(isbn: String): Boolean =
-        books[isbn] != null
-
-    override fun save(book: Book): Book =
-        book.let {
-            books.put(it.isbn, it)
-            it
-        }
-
-    override fun deleteByIsbn(isbn: String) {
-        books.remove(isbn)
-    }
-
-}
+// @Repository
+// class InMemoryBookRepository : BookRepository {
+//     companion object {
+//         private val books = ConcurrentHashMap<String, Book>()
+//     }
+//
+//     override fun findAll(): Iterable<Book> =
+//         books.values
+//
+//     override fun findByIsbn(isbn: String): Book? =
+//         books[isbn]
+//
+//     override fun existsByIsbn(isbn: String): Boolean =
+//         books[isbn] != null
+//
+//     override fun save(book: Book): Book =
+//         book.let {
+//             books.put(it.isbn, it)
+//             it
+//         }
+//
+//     override fun deleteByIsbn(isbn: String) {
+//         books.remove(isbn)
+//     }
+//
+// }
